929fbfd17bff17b43c3575ebb6179f0cba226372
Alex Tomlins <alex@tomlins.org.uk>
2012-07-23 13:27:27 +0100

    Add yardocs for Plugboard

--- lib/enigma_machine/plugboard.rb
+++ lib/enigma_machine/plugboard.rb
@@ -1,16 +1,34 @@
 class EnigmaMachine
   class Plugboard
-    def initialize(mapping_pairs, decorated)
+    # Construct a new plugboard
+    #
+    # Example:
+    #
+    #   Plugboard.new(%w(AB CD EF GH), @rotor)
+    #
+    # @param mapping_pairs [Array<String>] A list of letter pairs to be connected
+    # @param rotor [Rotor] the rightmost rotor that will be called next in the processing chain
+    def initialize(mapping_pairs, rotor)
       build_mapping(mapping_pairs)
-      @decorated = decorated
+      @decorated = rotor
     end
 
+    # Translate a letter
+    #
+    # This performs a substitution, calls the rotor to do the rest of the translation, and then
+    # substitutes the result on the way back out.
+    #
+    # @param letter [String] the letter to be translated
+    # @return [String] the translated letter
     def translate(letter)
       step = substitute(letter)
       step = @decorated.translate(step)
       substitute(step)
     end
 
+    # Substitutes a letter according the configured plug pairs
+    # @param letter [String] the letter to be substituted
+    # @return [String] the substituted letter
     def substitute(letter)
       @mapping[letter] || letter
     end
