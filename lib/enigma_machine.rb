e89fda2dc9d4179d4d32df0689da8a3a0a53d9a8
Alex Tomlins <alex@tomlins.org.uk>
2012-07-23 21:07:41 +0100

    Docs for EnigmaMachine class

--- lib/enigma_machine.rb
+++ lib/enigma_machine.rb
@@ -3,6 +3,45 @@ class EnigmaMachine
 
   ALPHABET = ('A'..'Z').to_a
 
+  # Constructs an enigma machine with given reflector, rotors and plugboard settings.
+  #
+  # Examples:
+  #
+  # You can use the standard reflectors and rotors:
+  #
+  #   EnigmaMachine.new(
+  #     :reflector => :B,
+  #     :rotors => [[:i, 10], [:ii, 14], [:iii, 21]],
+  #     :plug_pairs => %w(AP BR CM FZ GJ IL NT OV QS WX)
+  #   )
+  #
+  # Or you can use custom reflector and rotor configurations:
+  #
+  #   EnigmaMachine.new(
+  #     :reflector => %w(AF BV CP DJ EI GO HY KR LZ MX NW TQ SU),
+  #     :rotors => [
+  #       ['BDFHJLCPRTXVZNYEIWGAKMUSQO_V', 10],
+  #       ['JPGVOUMFYQBENHZRDKASXLICTW_MZ', 14],
+  #       ['ABCDEFGHIJKLMNOPQRSTUVWXYZ_AHT', 21]
+  #     ],
+  #     :plug_pairs => %w(AP BR CM FZ GJ IL NT OV QS WX)
+  #   )
+  #
+  # When specifying a custom rotor, the letters after the _ indicate the notch positions.
+  #
+  # @param config [Hash] A Hash of configuration params
+  # @option config [Symbol,Array<String>] :reflector Which reflector to use.  Reference one of the
+  #   standard ones by Symbol, or pass in an array of
+  #   letter pairs for a custom configuration.
+  # @option config [Array<Array>] :rotors Array of details for 3 or more rotors.  Specified from
+  #   left to right. Each one is specified as a tuple of the rotor spec,
+  #   and ring setting.  The rotor_spec can be one of the standard rotors
+  #   referenced by Symbol, or a custom rotor described by a config string
+  #   (see Examples).
+  # @option config [Array<String>] :plug_pairs an Optional array of letter pairs specifying the plugboard
+  #   configuration.  If omitted, no plugboard substitutions will be performed.
+  #
+  # @raise [ConfigurationError] if an invalid configuration was specified
   def initialize(config)
     @reflector = Reflector.new config[:reflector]
     @rotors = []
@@ -12,17 +51,35 @@ class EnigmaMachine
     @plugboard = Plugboard.new(config[:plug_pairs] || [], @rotors.last)
   end
 
+  # Set the rotors to the given positions.
+  #
+  # Pass in Positions for the rotors from left to right
+  #
+  # @param positions [*String] the positions to set the rotors to
+  # @return [void]
   def set_rotors(*positions)
     positions.each_with_index do |position, i|
       @rotors[i].position = position
     end
   end
 
+  # Simulates pressing a given key on the machine keyboard.
+  #
+  # Advances the rotors, and then translates the letter
+  #
+  # @param letter [String] the letter to be translated
+  # @return [String] the translated letter
   def press_key(letter)
     advance_rotors
     @plugboard.translate(letter)
   end
 
+  # Translates the given message using the current settings
+  #
+  # This will pass through space and - unmodified, and discard any other non-alpha characters.
+  #
+  # @param message [String] the message to be translated
+  # @return [String] the translated message
   def translate(message)
     message.upcase.each_char.map do |letter|
       case letter
