561cd8f79299ef089e39d9a5f8980c772b3feaf8
Alex Tomlins <alex@tomlins.org.uk>
2012-07-25 14:16:31 +0100

    Added docs for the Rotor class

--- lib/enigma_machine/rotor.rb
+++ lib/enigma_machine/rotor.rb
@@ -13,6 +13,20 @@ class EnigmaMachine
       :gamma  => "FSOKANUERHMBTIYCWLQPZXVGJD_", # in position 4, and hence have no notches.
     }
 
+    # Construct a new rotor
+    #
+    # Examples:
+    #
+    #   Rotor.new(:ii, 12, @next)
+    #   Rotor.new("BDFHJLCPRTXVZNYEIWGAKMUSQO_V", 15, @next)
+    #
+    # When specifying a custom rotor_spec, the letters after the _ indicate the notch positions
+    #
+    # @param rotor_spec [Symbol, String] A symbol representing one of the standard rotors or
+    #   a string specifying the mapping and notch positions (see Examples)
+    # @param ring_setting [Integer] The ring setting for the rotor.
+    # @param decorated [#translate] The next component in the processing chain (the next rotor, or the reflector)
+    # @raise [ConfigurationError] if an invalid rotor_spec is passed in (unrecognised standard rotor)
     def initialize(rotor_spec, ring_setting, decorated)
       if rotor_spec.is_a?(Symbol)
         raise ConfigurationError unless STANDARD_ROTORS.has_key?(rotor_spec)
@@ -26,33 +40,56 @@ class EnigmaMachine
       self.position = 'A'
     end
 
+    # Set the position of the rotor
+    #
+    # @param letter [String] the letter position to set the rotor to
+    # @return [void]
     def position=(letter)
       @position = ALPHABET.index(letter)
     end
+    # @return [String] the current position of the rotor
     def position
       ALPHABET[@position]
     end
 
+    # Advance the position of the rotor.
+    # @return [void]
     def advance_position
       @position = (@position + 1).modulo(26)
     end
 
+    # Is the rotor currently at a notch position.
     def at_notch?
       @notch_positions.include?(self.position)
     end
 
+    # Perform the forward translation of a letter (i.e. the path towards the reflector)
+    #
+    # @param letter [String] the letter to be translated
+    # @return [String] the translated letter
     def forward(letter)
       index = add_offset ALPHABET.index(letter)
       new_index = sub_offset @mapping[index]
       ALPHABET[new_index]
     end
 
+    # Perform the reverse translation of a letter (i.e. the path returning from the reflector)
+    #
+    # @param letter [String] the letter to be translated
+    # @return [String] the translated letter
     def reverse(letter)
       index = add_offset ALPHABET.index(letter)
       new_index = sub_offset @mapping.index(index)
       ALPHABET[new_index]
     end
 
+    # Translate a letter
+    #
+    # This performs a forward substitution, calls the next component to do the rest of the translation, and then
+    # reverse substitutes the result on the way back out.
+    #
+    # @param letter [String] the letter to be translated
+    # @return [String] the translated letter
     def translate(input)
       step = forward(input)
       step = @decorated.translate(step)
